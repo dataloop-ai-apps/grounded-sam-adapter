b9fb09765ed8e2af3401a15985b2f880c09672f0
ShadiMah <shadi.m@dataloop.ai>
2024-02-26 21:47:12 +0200

    Grounded SAM Adapter upload

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM dataloopai/dtlpy-agent:cpu.py3.8.pytorch2
+USER root
+RUN apt update
+
+USER 1000
+
+COPY ./ /tmp/app
+WORKDIR /tmp/app
+
+RUN pip3 install --user -r _requirements.txt
+
+
+# docker build --no-cache -t gcr.io/viewo-g/piper/agent/runner/cpu/grounded-sam:0.1.1 -f Dockerfile .
+# docker push gcr.io/viewo-g/piper/agent/runner/cpu/grounded-sam:0.1.1
---
+++ tests/test_sam.py
@@ -0,0 +1,31 @@
+import dtlpy as dl
+from adapters.sam_adapter.sam_adapter import SegmentAnythingAdapter
+
+
+def test_local_predict_item():
+    # item = dl.items.get(item_id='6493bdb47614d538df036ae9')
+    item = dl.items.get(item_id='646e04519cabfb4fc7e61500')
+    model_entity = dl.models.get(model_id='64c0dfd3319ec44dd2866635')
+    gs = SegmentAnythingAdapter(model_entity=model_entity)
+    gs.configuration['output_type'] = 'box'
+    gs.configuration['points_per_side'] = 4
+    annotations = gs.predict_items([item])
+
+
+# def test_remote_adapter():
+#     model_entity = dl.models.get(model_id='6493be8188a93d01b79c1d9c')
+#     adapter = model_entity.package.build(module_name='model-adapter', init_inputs={'model_entity': model_entity})
+#     item = dl.items.get(item_id='6493bdb47614d538df036ae9')
+#     adapter.configuration['classes'] = ['tide', 'orange']
+#     adapter.configuration['with_nms'] = False
+#     adapter.configuration['box_threshold'] = 0.1
+#     adapter.configuration['text_threshold'] = 0.1
+#     adapter.configuration['min_area'] = 100
+#     adapter.configuration['max_area'] = 10000
+#     annotations = adapter.predict_items([item])
+
+
+if __name__ == "__main__":
+    ...
+    dl.setenv('prod')
+    test_local_predict_item()
