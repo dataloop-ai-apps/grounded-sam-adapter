d4d1f4c271041622389fedb81e578702b6b6b6c7
ShadiMah <shadi.m@dataloop.ai>
2024-08-08 15:34:42 +0300

    test

--- adapters/grounded_sam2_adapter/grounded_sam2_adapter.py
+++ adapters/grounded_sam2_adapter/grounded_sam2_adapter.py
@@ -34,10 +34,13 @@ class GroundedSam2(GroundedSamBase):
 
 
 if __name__ == '__main__':
-    dl.setenv('rc')
-    item = dl.items.get(item_id='648e90d94607411277972ad7')
-    model = dl.models.get(model_id='66b33a35002458f33db87350')
-    model.name = 'grounded_sam2_adapter'
-    model.configuration = {}
+    # dl.setenv('rc')
+    dataset = dl.datasets.get(dataset_id='66a8e530c47183518d08d3d7')
+    filters = dl.Filters()
+    filters.add(field='metadata.system.mimetype', values='image*')
+    items = dataset.items.list(filters=filters)
+    items = items.items
+    model = dl.models.get(model_id='66b490c6595c48943d5b4a85')
     adapter = GroundedSam2(model_entity=model)
-    adapter.predict_items(items=[item])
+    adapter.predict_items(items=items)
+dl.Item.download()
